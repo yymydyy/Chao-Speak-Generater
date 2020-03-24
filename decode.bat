36ba10dcf0323ae2d5851e962194ea8d07630080
yymydyy <yymydyy@users.noreply.github.com>
2020-03-24 15:43:13 +0800

    V1.1
    Fix  a cmd waring bug.

--- decode.bat
+++ decode.bat
@@ -1,6 +1,3 @@
 @echo off
 set /p in=<./in.txt
-base16 -d %in%>./out.txt
-::echo %in%
-::pause
-::base16 -d &^e
+base16 -d "%in%">./out.txt
--- encode.bat
+++ encode.bat
@@ -1,6 +1,3 @@
 @echo off
 set /p in=<./in.txt
-base16 -e %in%>./out.txt
-::echo %in%
-::pause
-::base16 -d &^e
+base16 -e "%in%">./out.txt
